e303db89c5508c1d8a291845f1c19798f935cdab
ToLu-dev <omkar.nate1@gmail.com>
2019-12-13 00:00:10 +0530

    two pages updated
    About-Us completed
    Contact-Us: still have to integrate google maps in the component

--- CDAC-Project-POLD/src/app/components/nav-bar/nav-bar.component.ts
+++ CDAC-Project-POLD/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,14 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgModule } from '@angular/core';
+import { AboutUsComponent } from 'src/app/components/about-us/about-us.component';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
+
+@NgModule({
+  declarations:[NavBarComponent,AboutUsComponent]
+})
 export class NavBarComponent implements OnInit {
 
   constructor() { }
-
   ngOnInit() {
   }
 

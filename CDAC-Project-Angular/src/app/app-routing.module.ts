c4a6f0b4e34ebe2b0110d6d909c5a94122b38f44
pranavlengule77 <pranavlengule77@gmail.com>
2019-12-28 02:56:59 +0530

    updated new

--- CDAC-Project-Angular/src/app/app-routing.module.ts
+++ CDAC-Project-Angular/src/app/app-routing.module.ts
@@ -1,24 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-<<<<<<< HEAD
 import {ContactUsComponent} from './contact-us/contact-us.component';
-//import { userInfo } from 'os';
-=======
 import { BodyComponent } from './body/body.component';
->>>>>>> ec4daadb835a156309679d204a57e71fa5b35094
 
 const routes: Routes = [
   {
     path:'contact',
     component:ContactUsComponent
-
-<<<<<<< HEAD
-=======
-const routes: Routes = [
+  },
   {
     path:'',
     component: BodyComponent
->>>>>>> ec4daadb835a156309679d204a57e71fa5b35094
   }
 ];
 
--- CDAC-Project-Angular/src/app/app.module.ts
+++ CDAC-Project-Angular/src/app/app.module.ts
@@ -3,20 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { ContactUsComponent } from './contact-us/contact-us.component';
-=======
 import { BodyComponent } from './body/body.component';
->>>>>>> ec4daadb835a156309679d204a57e71fa5b35094
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    ContactUsComponent
-=======
+    ContactUsComponent,
     BodyComponent
->>>>>>> ec4daadb835a156309679d204a57e71fa5b35094
   ],
   imports: [
     BrowserModule,

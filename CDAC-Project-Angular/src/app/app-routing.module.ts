76bd291ed38e94db6b8af4d2c0bad20904799345
cdac-project-pold <58602666+cdac-project-pold@users.noreply.github.com>
2019-12-28 01:35:39 +0530

    created contact component

--- CDAC-Project-Angular/src/app/app-routing.module.ts
+++ CDAC-Project-Angular/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ContactUsComponent} from './contact-us/contact-us.component';
+//import { userInfo } from 'os';
 
+const routes: Routes = [
+  {
+    path:'contact',
+    component:ContactUsComponent
 
-const routes: Routes = [];
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
